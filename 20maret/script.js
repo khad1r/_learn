80b36a928f935127ff490586d684e45ec929e80e
khad1r <khadjahel@gmail.com>
2020-03-19 20:20:43 +0700

    y

--- 20maret/script.js
+++ 20maret/script.js
@@ -1,17 +1,28 @@
-$(".btn").onclick = scrape();
-
 function scrape() {
-    const url = document.getElementById("url").value.split("?");
+    const link = document.getElementById("link").value.split("/");
+    console.log(link);
+    url = "https://www.instagram.com/p/" + link[4] + "/?__a=1";
     console.log(url);
     $.ajax({
-        url: url[0] + "?__a=1",
+        url: url,
         type: 'get',
         success: function (post) {
             console.log(post);
             image = post.graphql.shortcode_media.display_url;
             // window.open(url_image);
             image_html = '<img src="' + image + '"style="width:200px">';
-            $(".image").append(image_html);
+            $(".scrape").append(image_html);
+            return;
+        }
+    });
+    url = "https://www.instagram.com/" + link[4] + "/?__a=1";
+    console.log(url);
+    $.ajax({
+        url: url,
+        type: 'get',
+        success: function (story) {
+            console.log(story);
+            return;
         }
     });
 }
